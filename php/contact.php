621328a4d713edd109fa3b130ed30a86fe7fe361
Jmussardev <jmussard.dev@gmail.com>
2020-11-11 17:26:55 +0100

    essai css formulaire - à recommencer

--- php/contact.php
+++ php/contact.php
@@ -23,9 +23,66 @@
 
 </section>
 <section class="section_part section_part_right">
+    
+<header class='section_part_right_header'>
+    <h1 class='section_part_right_header_title'> Contact </h1>
+    </header>
+    <div class='sectionRight_mainBlock'>
+        <form method="POST" action="#" class='sectionRight_formulaire'>
+        <fieldset class='fieldset1'>
+            <legend>Identité</legend>
+           
+            <p class='formLabels'> Je suis </br>
+                Mon prénom est </br>  
+                Et mon nom,</br> 
+                J'ai connu le site grâce à </p>
+           
+            <div class='formInputs'>
+                <p>
+                <input type='radio' id='woman' name='sexe' value='woman'>
+                 <label for='woman'> une femme /</label>
+                <input type='radio' id='man' name='sexe' value='man'>
+                 <label for='man'> un homme </label>
+          
+                <input class='inputChamps' type='text' name='prenom' id='prenom' placeholder="Prénom">
+                 <input class='inputChamps' type='text' name='nom' id='nom' placeholder="Nom">
+                  
+                 <select name="médias" class='inputChamps'>
+                      <option value="">choisir</option>
+                      <option value="Facebook">Facebook</option>
+                      <option value="Twitter">Twitter</option>
+                      <option value="Google">Google</option>
+                      <option value="Bouche à oreilles" >Bouche à oreilles</option>
+                      <option value="JT de 13h de Jean-Pierre Pernault">JT de 13h de Jean-Pierre Pernault</option>
+                      <option value="Autre">Autre</option>
+                 </select></p>
+              
+            </div>
+            
+        </fieldset>
+        <fieldset class='fieldset1'>
+            <legend>Message</legend>
+            <p class='formLabels'>Répondez-moi via</br>
+             Je voulais vous dire que<p> 
+                <div class='formInputs2'>
+                <p> <input type="mail" name='mail' placeholder="Adresse e-mail" class='inputChamps'>
+             <textarea name='message' class='inputChamps' rows="4"> </textarea></p>
+             <!-- Et vous montrer ça aussi -->
+           <input type='file' name='fichier'></div>
+        </fieldset>
+
+
+        <fieldset>
+            <p>Je certifie la véracité de ces informations. <input type='checkbox' name='certification'> </p>
+            
+        </fieldset>
+
+        <p> <input type='submit' value="Envoyer"> </p>
+
+        </form>
+    </div>
+
 </section>
-
-
 
 
 
